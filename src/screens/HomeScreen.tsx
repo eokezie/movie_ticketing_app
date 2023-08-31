512cc4a8352b4eccf4085ed5e8e79f51c71d3af3
Emeka Okezie <eokezie@nasdng.com>
2023-08-31 02:07:56 +0100

    App Setup with font properties and colors

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { COLORS } from '../theme/theme';
+
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>HomeScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: COLORS.Black
+  }
+});
+
+export default HomeScreen;
---
+++ src/screens/MovieDetailsScreen.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+
+const MovieDetailsScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>MovieDetailsScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {}
+});
+
+export default MovieDetailsScreen;
